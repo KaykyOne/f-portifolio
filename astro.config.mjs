95f2ed66c1032954aba41679003c935e56319a2a
KaykyOne <kaykyzioti@gmail.com>
2025-06-02 19:40:44 -0300

    vamos nessa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+const site = 'https://KaykyOne.github.io/f-portifolio'; 
+
 // https://astro.build/config
 export default defineConfig({
+  site, 
+  base: '/f-portifolio/', // nome do repositório
   vite: {
     plugins: [tailwindcss()],
   },
